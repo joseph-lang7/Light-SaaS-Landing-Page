010021ed4e35051b68ff1263cacd082a21535355
Joseph Lang <143908150+joseph-lang7@users.noreply.github.com>
2024-09-03 20:27:11 -0700

    Add container to text section in product showcase

--- src/sections/product-showcase/product-showcase.tsx
+++ src/sections/product-showcase/product-showcase.tsx
@@ -2,22 +2,25 @@ import productImage from "@/assets/product-image.png";
 import Image from "next/image";
 import pyramidImage from "@/assets/pyramid.png";
 import donutImage from "@/assets/donut.png";
+import { Container } from "@/components/container/container";
 export const ProductShowcase = () => {
   return (
     <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
-      <div className="max-w-[540px] mx-auto">
-        <div className="flex justify-center">
-          <div className="tag">Boost your productivity</div>
+      <Container>
+        <div className="max-w-[540px] mx-auto">
+          <div className="flex justify-center">
+            <div className="tag">Boost your productivity</div>
+          </div>
+          <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+            A more effective way to track progress
+          </h2>
+          <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+            Effortlessly turn your ideas into a fully functional, responsive,
+            no-code SaaS website in just minutes with the set of free components
+            for Framer.
+          </p>
         </div>
-        <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
-          A more effective way to track progress
-        </h2>
-        <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
-          Effortlessly turn your ideas into a fully functional, responsive,
-          no-code SaaS website in just minutes with the set of free components
-          for Framer.
-        </p>
-      </div>
+      </Container>
       <div className="flex justify-center">
         <div className="relative">
           <Image src={productImage} alt="Product" className="mt-10" />
