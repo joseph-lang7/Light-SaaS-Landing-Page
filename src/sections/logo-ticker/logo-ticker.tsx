b57e9dbba2977f4886371cfa69e61b3fd96cc561
Joseph Lang <143908150+joseph-lang7@users.noreply.github.com>
2024-09-19 12:02:52 -0700

    center logo ticker on large viewports

--- src/sections/logo-ticker/logo-ticker.tsx
+++ src/sections/logo-ticker/logo-ticker.tsx
@@ -5,22 +5,22 @@ import { logoTickerLogos } from "@/data";
 export const LogoTicker = () => {
   return (
     <section className="py-8 md:py-12 bg-white">
-      <div className="container">
+      <div className="container mx-auto">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
           <motion.div
-            className="flex gap-14 flex-none pr-14"
+            className="flex flex-none gap-14 mr-14"
             animate={{
               translateX: "-50%",
             }}
             transition={{
-              duration: 10,
+              duration: 20,
               repeat: Infinity,
               ease: "linear",
               repeatType: "loop",
             }}
           >
             {[
-              ...new Array(2).fill(0).map((_, idx) => (
+              ...new Array(4).fill(0).map((_, idx) => (
                 <>
                   {logoTickerLogos.map((logo, idx) => (
                     <Image
