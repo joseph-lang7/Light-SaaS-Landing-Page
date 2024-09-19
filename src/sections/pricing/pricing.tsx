4f59f03b4b03db09214c7210c6b4e3afe57d02c0
Joseph Lang <143908150+joseph-lang7@users.noreply.github.com>
2024-09-19 11:01:05 -0700

    Animate popular tag in pricing section and hover animations for sign up buttons

--- src/sections/pricing/pricing.tsx
+++ src/sections/pricing/pricing.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { Container } from "@/components/container/container";
 import { CheckIcon } from "@/components/icons/icons";
 import { pricingTiers } from "@/data";
 import { pricingTierType } from "@/data";
+import { motion } from "framer-motion";
 export const Pricing = () => {
   return (
     <section className="py-24 bg-white">
@@ -33,9 +35,20 @@ export const Pricing = () => {
                   <h3 className="text-lg font-bold text-black/50">{title}</h3>
                   {popular && (
                     <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
-                      <span className="bg-[linear-gradient(to_right,#DD7DDf,#E1CD86,#BBCB92,#71C2Ef,#3BFFFF,#DD7DDf)] bg-clip-text text-transparent font-medium">
+                      <motion.span
+                        animate={{
+                          backgroundPositionX: "-100%",
+                        }}
+                        transition={{
+                          duration: 1,
+                          repeat: Infinity,
+                          ease: "linear",
+                          repeatType: "loop",
+                        }}
+                        className="bg-[linear-gradient(to_right,#DD7DDf,#E1CD86,#BBCB92,#71C2Ef,#3BFFFF,#DD7DDf,#DD7DDf,#E1CD86,#BBCB92,#71C2Ef,#3BFFFF,#DD7DDf)] [background-size:200%] bg-clip-text text-transparent font-medium"
+                      >
                         Popular
-                      </span>
+                      </motion.span>
                     </div>
                   )}
                 </div>
